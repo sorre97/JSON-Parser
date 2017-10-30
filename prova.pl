5a642bf88be43e6ed9e7c4c0384caeb46d6fad94
Alessandro <sorre97@hotmail.it>
2017-10-30 15:07:13 +0100

    Update prova.pl

--- prova.pl
+++ prova.pl
@@ -1,14 +1,3 @@
 %%% -*- Mode: prolog -*-
 
-project([], []).
-project([X | Rest], [X1 | Rest1]) :-
-    functor(X, _, Arity),
-    Arity > 1,
-    !,
-    arg(2, X, X1),
-    project(Rest, Rest1).
-project([X | Rest], [X1 | Rest1]) :-
-    functor(X, _, Arity),
-    Arity < 2,
-    !,
-    project(Rest, [X1 | Rest1]).
+cacca
