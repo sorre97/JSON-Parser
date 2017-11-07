b4f8874304f08b4a990c169227216113e0b73a97
sorre97 <Soldier@macusers-MacBook-Pro.local>
2017-11-07 16:28:12 +0100

    JSON-Parser

--- JSON-Parser.pl
+++ JSON-Parser.pl
@@ -4,10 +4,10 @@
 
 % JSON parse definition
 % json_parse/2
-json_parse(String) :-
+json_parse(String, JSONString) :-
     string_codes(String, StringCodes),
     is_JSON(StringCodes).
-
+    
 % JSON object definition
 % is_JSON/1
 is_JSON(X) :-
@@ -24,33 +24,68 @@ is_object([X, Y]) :-
     X = 0'{,
     Y = 0'},
     !.
-
+    
 is_object(AsciiList) :-
     AsciiList = [0'{ | Rest],
     delete_last(Rest, RestModified, LastElement),
     LastElement = 0'},
-    is_member(RestModified),
+    is_members(RestModified),
     !.
-
+    
 % MEMBERS definition
 % is_members/1
 is_members(AsciiList) :-
-    is_pair(AsciiList). %__________________________________________________________continua qui
+    is_pair(AsciiList, [0', | Rest]),
+    !,  % to check
+    is_member(Rest).
+    
+is_members(AsciiList) :-
+    is_pair(AsciiList, []),
+    !. % to check
+    
+% PAIR definition
+% is_pair/2 
+is_pair(AsciiList, Rest) :-
+    is_string(AsciiList, [0': | Rest2]),
+    is_string(Rest2, Rest). %todo<------
+    %is_value(Rest2, Rest).
+    
 
-%%%% Helper Functions Definition
+% STRING definition
+% is_string/2
+is_string([0'" | AsciiList], Rest) :-
+        skip_chars(AsciiList, Rest).
+        
+
+
+% VALUE definition
+% is_value/2
+
+
+%%%% Helper Functions Definitions
 
 % Delete Last definition
 % delete_last/3
-/*This predicate operates on List and
-returns two elements, Ris which is
-List without the last element and
-Element which is the removed element	*/
-
+/*  This predicate operates on List and 
+        returns two elements, Ris which is 
+        List without the last element and
+        Element which is the removed element    */
+    
 delete_last(List, Ris, Element) :-
     reverse(List, [Element | T]),
     reverse(T, Ris).
+    
+% SKIP CHARS
+% skip_chars/2
+
+skip_chars([X | Xs], Ris) :-
+    X \= 0'",
+    !,
+    skip_chars(Xs, Ris).
+    
+skip_chars([X | Xs], Xs) :-
+    X = 0'",
+    !.
 
 %%%% End JSON-Parser.pl
-%
-%   % is_pair ritorna due valori, l'oggetto parsato e una lsita che contiene tutto ciò che c'è dopo la , compresa, se il primo elemento è una virgola
-% richiamo is_member, se ho lista vuota ho solo un pair e se non ho nessuno dei due casi fallisco, ho es 123.123a
+
