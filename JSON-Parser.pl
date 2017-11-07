9e417db2ca790ea9f7c3f5b16e3e6b7295eb21f9
claudiom4sir <claudio_rota@hotmail.it>
2017-11-07 20:12:48 +0100

    MODIFICA CLAUDIO
    aggiunta commenti "da implementare" e sistemazione indentazione

--- JSON-Parser.pl
+++ JSON-Parser.pl
@@ -15,7 +15,7 @@ is_JSON(X) :-
     !.
 
 /*
-is_JSON(X) :-
+is_JSON(X) :-	%***************** DA IMPLEMENTARE *****************
     is_array(X),
     !.
 */
@@ -51,37 +51,34 @@ is_pair(AsciiList, Rest) :-
     is_string(AsciiList, [0': | Rest2]),
     is_value(Rest2, Rest).
     
-
 % STRING definition
 % is_string/2
 is_string([0'" | AsciiList], Rest) :-
-        skip_chars(AsciiList, Rest).
+    skip_chars(AsciiList, Rest).
         
-
 % VALUE definition
 % is_value/2
 is_value(AsciiList, Rest) :-
-        is_string(AsciiList, Rest),
+	is_string(AsciiList, Rest),
     !.
     
 is_value(AsciiList, Rest) :-
-        is_number(AsciiList, Rest).
+    is_number(AsciiList, Rest).
     
-
 /* 
-is_value(AsciiList, Rest) :-
-        is_JSON(AsciiList, Rest),
+is_value(AsciiList, Rest) :-	%***************** DA IMPLEMENTARE *****************
+    is_JSON(AsciiList, Rest),
     !.
 */
 
 % NUMBER definition
 % is_number/2
 is_number(AsciiList, Rest) :-
-        parse_int(AsciiList, Num, MoreInput),
+    parse_int(AsciiList, Num, MoreInput),
     !.
     
 is_number(AsciiList, Rest) :-
-        parse_float(AsciiList, Num, MoreInput),
+    parse_float(AsciiList, Num, MoreInput),
     !.
 
 %%%% Helper Functions Definitions
@@ -89,9 +86,9 @@ is_number(AsciiList, Rest) :-
 % Delete Last definition
 % delete_last/3
 /*  This predicate operates on List and 
-        returns two elements, Ris which is 
-        List without the last element and
-        Element which is the removed element    */
+    returns two elements, Ris which is 
+	List without the last element and
+    Element which is the removed element    */
     
 delete_last(List, Ris, Element) :-
     reverse(List, [Element | T]),
@@ -99,7 +96,6 @@ delete_last(List, Ris, Element) :-
     
 % SKIP CHARS
 % skip_chars/2
-
 /*  This predicate skips every char up to the next double quote sign */
     
 skip_chars([X | Xs], Ris) :-
@@ -114,36 +110,35 @@ skip_chars([X | Xs], Xs) :-
 % Skip White Spaces definition
 % skip_white/2
 /*  This predicate recives a list, skips every space
-        up to the first encountered char and returns the modified list*/
+    up to the first encountered char and returns the modified list*/
         
 skip_white([X | List], List1) :-
-        char_type(X, white),
+    char_type(X, white),
     !,
     skip_white(List, List1).
   
 skip_white(List, List).
         
-    
-% Parse Int definition
-% parse_int/3
-% parse_int/5
+% Parse_int - float definition
+% parse_int-float/3
+% parse_int1/3
 /*  This predicate parses the integer and returns everything
-        that is not a number in Moreinput */
+    that is not a number in Moreinput */
 
 parse_int(List, Integer, MoreInput) :-
-        skip_white(List, List1),
+    skip_white(List, List1),
     parse_int1(List1, ListNum, MoreInput),
     number_codes(Integer, ListNum).
     
 parse_int1([X | Xs], [X | Acc], MoreInput) :-
-        is_digit(X),
-        !,
+    is_digit(X),
+	!,
     parse_int1(Xs, Acc, MoreInput).
     
 parse_int1(MoreInput, [], MoreInput).
 
 parse_float(List, Float, MoreInput) :-
-        skip_white(List, List1),
+	skip_white(List, List1),
     parse_int1(List1, IntegerCodes, [0'. | Rest]),
     parse_int1(Rest, DecimalCodes, MoreInput),
     IntegerCodes \= [],
